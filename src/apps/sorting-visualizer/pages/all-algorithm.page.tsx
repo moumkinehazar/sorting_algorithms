2a9bd223a4bcfaf18b3f3933dd95a71c33910ef2
moumkinehhazar <moumkine.hazar@gmail.com>
2024-01-19 17:38:00 +0100

    delete

--- src/apps/sorting-visualizer/pages/all-algorithm.page.tsx
+++
@@ -1,7 +0,0 @@
-import AllAlgorithmLayout from '@/apps/sorting-visualizer/layouts/all-algorithm.layout';
-
-function AllAlgorithmPage() {
-  return <AllAlgorithmLayout />;
-}
-
-export default AllAlgorithmPage;
